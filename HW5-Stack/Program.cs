824922a0bb59f1a50edd8d52560f6bc23302f271
feschenko-nelya <feschenko_n_s@list.ru>
2025-04-06 18:54:43 +0300

    Класс OtusStack.

---
+++ HW5-Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using HW5;
+
+namespace HW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new OtusStack("a", "b", "c");
+
+            // size = 3, Top = 'c'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+
+            var deleted = s.Pop();
+
+            // Извлек верхний элемент 'c' Size = 2
+            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+            s.Add("d");
+
+            // size = 3, Top = 'd'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            // size = 0, Top = null
+            Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+
+            try
+            {
+                s.Pop();
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+    }
+}
---
+++ HW5-Stack/Stack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HW5
+{
+	public class OtusStack
+	{
+		private List<string> _stack = new();
+		private int _size = 0;
+		public int Size { get => _size; }
+		public string? Top
+		{
+			get
+			{
+				if (_size <= 0)
+				{
+					return null;
+				}
+
+				return _stack.ElementAt(_size - 1);
+            }
+		}
+
+		public OtusStack()
+		{
+		}
+
+		public OtusStack(params string[] strs)
+		{
+			for (int i = 0; i < strs.Length; ++i)
+			{
+				strs[i] = strs[i];
+
+				Add(strs[i]);
+			}
+		}
+
+		public void Add(string str)
+		{
+			_stack.Add(str);
+			++_size;
+
+        }
+
+		public string Pop()
+		{
+			if (_size <= 0)
+			{
+				throw new StackIsEmptyException();
+
+            }
+
+			--_size;
+
+			string result = _stack.Last();
+			_stack.RemoveAt(_size);
+
+			return result;
+		}
+
+	}
+
+	public class StackIsEmptyException : Exception
+	{
+		public StackIsEmptyException() : base("Стек пустой.")
+		{
+
+		}
+    }
+}
