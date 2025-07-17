4e57684cae459656e5d4601f3bea0381c73d7ea1
feschenko-nelya <feschenko_n_s@list.ru>
2025-07-17 22:32:04 +0300

    UserCommand => ToDoItem

---
+++ HW2/User/ToDoItem.cs
@@ -0,0 +1,49 @@
+﻿using HW3;
+using Otus.ToDoList.ConsoleBot;
+using Otus.ToDoList.ConsoleBot.Types;
+
+namespace HW2.User
+{
+    public enum ToDoItemState { None, Active, Completed };
+    public class ToDoItem : AbstractCommand
+    {
+        public Guid Id { get; init; }
+        private ToDoUser? User;
+        public string Name { get; init; }
+        public DateTime CreatedAt { get; init; }
+        public ToDoItemState State { get; private set; }
+        public DateTime? StateChangedAt { get; private set; }
+
+        public ToDoItem(string name) : base(null)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            State = ToDoItemState.Active;
+            StateChangedAt = DateTime.Now;
+        }
+        public void SetCompleted()
+        {
+            State = ToDoItemState.Completed;
+            StateChangedAt = DateTime.Now;
+        }
+
+        public override void Execute(ITelegramBotClient botClient, Message botMessage)
+        {
+            botClient.SendMessage(botMessage.Chat, Name);
+        }
+
+        public override string GetInfo()
+        {
+            return Name;
+        }
+
+        public override string GetCode()
+        {
+            return Name;
+        }
+        public override bool IsEnabled(long telegramUserId)
+        {
+            return true;
+        }
+    }
+}
