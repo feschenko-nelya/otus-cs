ff6f803a149f06f269693ad26bdd226c5592b383
feschenko-nelya <feschenko_n_s@list.ru>
2025-07-22 23:19:17 +0300

    Классы ошибок вынесены в отдельный файл

---
+++ HW2/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class TaskCountLimitException : Exception
+{
+    public TaskCountLimitException(int taskCountLimit)
+           : base($"Превышено максимальное количество задач равное {taskCountLimit}")
+    {
+    }
+}
+public class TaskLengthLimitException : Exception
+{
+    public TaskLengthLimitException(int taskLength, int taskLengthLimit)
+           : base($"Длина задачи ‘{taskLength}’ превышает максимально допустимое значение {taskLengthLimit}.")
+    {
+    }
+}
+public class DuplicateTaskException : Exception
+{
+    public DuplicateTaskException(string task)
+           : base($"Задача ‘{task}’ уже существует.")
+    {
+    }
+}
