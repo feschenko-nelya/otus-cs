e9b8beacf5c3b9cf0a959fb44d9a1babc708d5bd
feschenko-nelya <feschenko_n_s@list.ru>
2025-08-08 21:53:52 +0300

    Добавление репозитория IUserRepository

---
+++ HW2/Bot_User/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using HW2.User;
+
+namespace HW2.Bot_User
+{
+    internal interface IUserRepository
+    {
+        ToDoUser? GetUser(Guid userId);
+        ToDoUser? GetUserByTelegramUserId(long telegramUserId);
+        void Add(ToDoUser user);
+    }
+}
---
+++ HW2/Bot_User/InMemoryUserRepository.cs
@@ -0,0 +1,44 @@
+﻿using HW2.User;
+
+namespace HW2.Bot_User
+{
+    internal class InMemoryUserRepository : IUserRepository
+    {
+        private List<ToDoUser> _toDoUsers = new();
+        public void Add(ToDoUser user)
+        {
+            if (GetUser(user.UserId) != null)
+            {
+                return;
+            }
+
+            _toDoUsers.Add(user);
+        }
+
+        public ToDoUser? GetUser(Guid userId)
+        {
+            foreach (ToDoUser user in _toDoUsers)
+            {
+                if (user.UserId == userId)
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public ToDoUser? GetUserByTelegramUserId(long telegramUserId)
+        {
+            foreach (ToDoUser user in _toDoUsers)
+            {
+                if (user.TelegramUserId == telegramUserId)
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+    }
+}
