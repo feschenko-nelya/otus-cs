a5cb75f281f0464a4d38006120ad15f39a8a1e0d
feschenko-nelya <feschenko_n_s@list.ru>
2025-09-08 22:39:45 +0300

    Создание DataContext

---
+++ HW2/Infrastructure/DataAccess/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using HW2.Core.DataAccess;
+
+namespace HW2.Infrastructure.DataAccess
+{
+    internal class DataContextFactory : IDataContextFactory<ToDoDataContext>
+    {
+        public ToDoDataContext CreateDataContext()
+        {
+            string? pgPswd = Environment.GetEnvironmentVariable("PG_PSWD");
+
+            return new ToDoDataContext($"User ID=postgres;Password={pgPswd};Host=localhost;Port=5432;Database=ToDoList;");
+        }
+    }
+}
